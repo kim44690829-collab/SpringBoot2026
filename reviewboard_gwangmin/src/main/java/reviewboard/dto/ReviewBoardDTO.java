8c63dffef8c8e6a72dcc88beae9f18d301aee125
kim44690829-collab <kim44690829@gmail.com>
2026-02-04 20:38:09 +0900

    2026-02-04 update2

--- reviewboard_gwangmin/src/main/java/reviewboard/dto/ReviewBoardDTO.java
+++ reviewboard_gwangmin/src/main/java/reviewboard/dto/ReviewBoardDTO.java
@@ -1,5 +1,11 @@
 package reviewboard.dto;
 
 public class ReviewBoardDTO {
-
+	private int num; // auto_increment
+	private String title; 
+	private String writer; 
+	private int rating;
+	private String content;
+	private String reg_date; // 기본값 오늘, 현재시간
+	private int readCount; // 기본값 0
 }
--- reviewboard_gwangmin/src/main/java/reviewboard/service/ReviewBoardService.java
+++ reviewboard_gwangmin/src/main/java/reviewboard/service/ReviewBoardService.java
@@ -1,8 +1,42 @@
 package reviewboard.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import reviewboard.dto.ReviewBoardDTO;
+import reviewboard.mapper.ReviewBoardMapper;
 
 @Service
 public class ReviewBoardService {
-
+	
+	@Autowired
+	ReviewBoardMapper reviewBoardMepper;
+	
+	public int insertBoard() {
+		System.out.println("ReviewBoardService : insertBoard() 매서드 확인");
+		return reviewBoardMepper.insertBoard();
+	}
+	
+	public List<ReviewBoardDTO> allSelect(int startRow, int pageSize){
+		System.out.println("ReviewBoardService : allSelect() 매서드 확인");
+		return reviewBoardMepper.allSelect(startRow, pageSize);
+	}
+	
+	public ReviewBoardDTO oneSelect(int num) {
+		System.out.println("ReviewBoardService : oneSelect() 매서드 확인");
+		return reviewBoardMepper.oneSelect(num);
+	}
+	
+	public int updateBoard(ReviewBoardDTO rbdto) {
+		System.out.println("ReviewBoardService : updateBoard() 매서드 확인");
+		return reviewBoardMepper.updateBoard(rbdto);
+	}
+	
+	public int deleteBoard(int num) {
+		System.out.println("ReviewBoardService : deleteBoard() 매서드 확인");
+		return reviewBoardMepper.deleteBoard(num);
+	}
+	
 }
